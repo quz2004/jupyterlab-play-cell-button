3452c175aafecfad950ba82b1f76fd69178adfcd
karhide <thomas.rich@nanoporetech.com>
2020-09-25 13:19:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# jupyterlab-play-cell-button
+
+![Github Actions Status](https://github.com/epi2me-labs/jupyterlab-play-cell-button/workflows/Build/badge.svg)
+
+Adds a play button to cells in jupyterlab
+
+
+
+## Requirements
+
+* JupyterLab >= 2.0
+
+## Install
+
+```bash
+jupyter labextension install  @epi2melabs/jupyterlab-play-cell-button
+```
+
+## Contributing
+
+### Install
+
+The `jlpm` command is JupyterLab's pinned version of
+[yarn](https://yarnpkg.com/) that is installed with JupyterLab. You may use
+`yarn` or `npm` in lieu of `jlpm` below.
+
+```bash
+# Clone the repo to your local environment
+# Move to jupyterlab-play-cell-button directory
+
+# Install dependencies
+jlpm
+# Build Typescript source
+jlpm build
+# Link your development version of the extension with JupyterLab
+jupyter labextension install .
+# Rebuild Typescript source after making changes
+jlpm build
+# Rebuild JupyterLab after making any changes
+jupyter lab build
+```
+
+You can watch the source directory and run JupyterLab in watch mode to watch for changes in the extension's source and automatically rebuild the extension and application.
+
+```bash
+# Watch the source directory in another terminal tab
+jlpm watch
+# Run jupyterlab in watch mode in one terminal tab
+jupyter lab --watch
+```
+
+Now every change will be built locally and bundled into JupyterLab. Be sure to refresh your browser page after saving file changes to reload the extension (note: you'll need to wait for webpack to finish, which can take 10s+ at times).
+
+### Uninstall
+
+```bash
+
+jupyter labextension uninstall jupyterlab-play-cell-button
+```
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import {
+  INotebookTracker
+} from '@jupyterlab/notebook';
+
+import { Cell } from '@jupyterlab/cells';
+
+import { PanelLayout } from '@lumino/widgets';
+
+import {
+  CellPlayButton
+} from './widget'
+
+/**
+ * Initialization data for the jupyterlab-play-cell-button extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-play-cell-button',
+  requires: [INotebookTracker],
+  autoStart: true,
+  activate: (
+      app: JupyterFrontEnd,
+      nbTrack: INotebookTracker,
+  ) => {
+    nbTrack.currentChanged.connect(() => {
+
+      const notebookPanel = nbTrack.currentWidget;
+      const notebook = nbTrack.currentWidget.content;
+
+      notebookPanel.context.ready.then(async () => {
+
+        let currentCell: Cell = null;
+        let currentCellPlayButton: CellPlayButton = null;
+
+        nbTrack.activeCellChanged.connect(() => {
+
+          // Remove the existing play button from
+          // the previously active cell. This may
+          // well introduce bugs down the road and
+          // there is likely a better way to do this
+          if (currentCell) {
+            notebook.widgets.map((c: Cell) => {
+              const currentLayout = c.layout as PanelLayout;
+              currentLayout.widgets.map(w => {
+                if (w === currentCellPlayButton) {
+                  currentLayout.removeWidget(w)
+                }
+              })
+            });
+          }
+
+          const cell: Cell = notebook.activeCell;
+          const newButton: CellPlayButton = new CellPlayButton(
+              cell, notebookPanel.sessionContext);
+          (cell.layout as PanelLayout).addWidget(newButton);
+
+          // Set the current cell and button for future
+          // reference
+          currentCell = cell;
+          currentCellPlayButton = newButton;
+        });
+
+      });
+
+    })
+  }
+};
+
+export default extension;
